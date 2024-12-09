a358133a05ff15c9df3a531f49d8ddc1f39b176a
Adhil-Akbar <356adhil@gmail.com>
2024-12-09 07:32:01 +0530

    ⚙️

--- app/serviceWorker.js
+++ app/serviceWorker.js
@@ -4,7 +4,13 @@ export function register() {
       navigator.serviceWorker
         .register('/sw.js')
         .then((registration) => {
-          console.log('ServiceWorker registration successful');
+          console.log('ServiceWorker registration successful with scope:', registration.scope);
+
+          registration.addEventListener('updatefound', () => {
+            // A new service worker is being installed
+            const installingWorker = registration.installing;
+            console.log('A new service worker is being installed:', installingWorker);
+          });
         })
         .catch((err) => {
           console.log('ServiceWorker registration failed: ', err);
